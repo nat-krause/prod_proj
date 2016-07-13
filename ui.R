08ba83841570876c829319870434f47735528f7e
Nat-Krause <nkrause@nationaled.net>
2016-07-13 02:30:34 -0500

    updated ui.R file

--- ui.R
+++ ui.R
@@ -22,7 +22,15 @@ shinyUI(fluidPage(
                 mainPanel(
                   h3('Predicted birth weight'),
                   h4('Based on the data you have entered, the expected birth weight is'),
-                  verbatimTextOutput("prediction")
+                  verbatimTextOutput("prediction"),
+                  br(),
+                  br(),
+                  br(),
+                  br(),
+                  h3(span('Documentation', style = "color:blue")),
+                  div("Use the sliders at right to enter the age and height for the baby's parents. The estimated birth weight in pounds will update automatically.", style = "color:blue"),
+                  br(),
+                  div("The underlying data was collected for each new mother in a Child and Health Development Study. It comes from http://www.stat.berkeley.edu/users/statlabs/labs.html via the UsingR package. 726 observations were included to generate the model. Observations that did not include a valid value for all four variables were excluded.", style = "color:blue")
                 )
   )
 ))
